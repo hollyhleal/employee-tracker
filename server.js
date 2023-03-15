c5b4e44239c15fe5a2a13dee527e026dabc1ee3a
Holly Leal <94670410+hollyhleal@users.noreply.github.com>
2023-03-15 10:51:59 -0600

    add packages and dependencies

---
+++ server.js
@@ -0,0 +1,6 @@
+// Package to connect to your MySQL database and perform queries
+const mysql = require("mysql2");
+// Package to interact with the user via the command line
+const inquirer = require("inquirer");
+// Package to print MySQL rows to the console
+const cTable = require("console.table");
