076bf3e0835ac51b614f4db64606afe6a7b49903
Holly Leal <94670410+hollyhleal@users.noreply.github.com>
2023-03-16 21:24:33 -0600

    update with query statements for all functions in application

--- db/query.sql
+++ db/query.sql
@@ -1,4 +1,37 @@
--- You might want to use a separate file that contains functions for performing specific SQL queries you'll need to use. A constructor function or class could be helpful for organizing these.
+-- viewDepartments
+-- view all departments in department table
 SELECT * FROM department;
+
+-- view all roles in role table
 SELECT * FROM role;
+
+-- view all employees in employee table
 SELECT * FROM employee;
+
+-- viewRoles
+SELECT role.id, role.title, role.salary, department.name as department FROM role JOIN department ON role.department_id = department.id
+
+-- viewEmployees
+SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT (manager.first_name, " ", manager.last_name) AS manager FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id
+
+-- addDepartment
+INSERT INTO department SET name = ?
+
+-- addRole
+SELECT id as value, name FROM department
+
+INSERT INTO role (title, salary, department_id) VALUES ("${title}", "${salary}", "${department_id}")
+
+-- addEmployee
+SELECT id AS value, title AS name FROM role
+
+SELECT CONCAT(first_name, " ", last_name) AS name, id AS value FROM employee WHERE manager_id is null
+
+INSERT INTO employee SET first_name = ?, last_name = ?, role_id = ?, manager_id = ?
+
+-- updateRole
+SELECT CONCAT(first_name, " ", last_name) AS name, id AS value FROM employee
+
+SELECT id as value, title as name FROM role
+
+UPDATE employee SET role_id = ? WHERE id = ?
